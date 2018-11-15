0d9b17124d4d22167efb9509862784b156bb4d36
GeonLee <leegun2003@gmail.com>
2018-11-15 16:32:20 +0900

    User fix

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -134,7 +134,7 @@ class User extends BaseEntity {
   wikiImages: WikiImage[];
 
   @ManyToMany(type => Game, { nullable: true })
-  Games: Game[];
+  games: Game[];
 
   @CreateDateColumn()
   createdAt: string;
